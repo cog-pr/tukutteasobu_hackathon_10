238c2ccaaba9f0542f7102dfc9b4e737771a2a95
kagamihara <itakuumi@gmail.com>
2026-07-11 16:45:50 +0900

    Revert "Initial commit" 一旦リセット This reverts commit 1b3d272fa00bc12a95dc927679934a2e1dff365c.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { PrototypeApp } from './client/PrototypeApp'
 import { ScreenListPage } from './pages/ScreenListPage'
 import { ROUTES } from './routes'
 import './styles.css'
 
-function OriginalUiApp() {
+function App() {
   return (
     <HashRouter>
       <Routes>
@@ -20,10 +19,8 @@ function OriginalUiApp() {
   )
 }
 
-const showOriginalUi = new URLSearchParams(window.location.search).get('ui') === 'original'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {showOriginalUi ? <OriginalUiApp /> : <PrototypeApp />}
+    <App />
   </StrictMode>,
 )
